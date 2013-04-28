134064b567e177635dc953d761b394c644842794
LeoInoue <leonardoinoue@gmail.com>
2013-04-28 17:29:38 -0300

    Create create_table_pedido.sql

---
+++ br/com/locaweb/scripts/create_table_pedido.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE pedido 
+(
+  id_pedido INT NOT NULL AUTO_INCREMENT,
+	tipo_produto VARCHAR(100) NOT NULL,
+	quantidade INT NOT NULL,
+	valor_unitario DOUBLE NOT NULL,
+	id_cliente INT,
+	PRIMARY KEY (id_pedido),
+	FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente)
+)
