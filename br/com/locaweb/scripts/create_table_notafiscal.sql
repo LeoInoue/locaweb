19e014ac397bbc07c0d76498fcf551c4478c05cd
LeoInoue <leonardoinoue@gmail.com>
2013-04-28 17:29:13 -0300

    Create create_table_notafiscal.sql

---
+++ br/com/locaweb/scripts/create_table_notafiscal.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE nota_fiscal
+(
+  id_nota INT NOT NULL AUTO_INCREMENT,
+	data_emissao DATE NOT NULL,
+	valor_bruto DOUBLE NOT NULL,
+	valor_imposto DOUBLE NOT NULL,
+	id_cliente INT,
+	PRIMARY KEY (id_nota),
+	FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente)	
+)
